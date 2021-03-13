50eb1af1f0bb269ce2f4a80bcbf1e81450f3f796
Ben Gross <benrgross@gmail.com>
2021-03-12 20:59:31 -0500

    wrapper and title components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
+import Title from "./components/Title/Title";
+import Wrapper from "./components/Wrapper/index";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    employees: null,
+  };
+
+  componentDidMount() {
+    // fetch some dad jokes
+    this.getDadJoke();
+  }
+
+  getEmployees = async () => {
+    const { data } = await axios.get("https://randomuser.me/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    this.setState({ employees: data });
+  };
+
   render() {
+    console.log(this.state.employees);
     return (
-      <div>
-        <p>hello</p>
-      </div>
+      <Wrapper>
+        <Title />
+      </Wrapper>
     );
   }
 }
---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export const Title = () => {
+  return (
+    <div className="nav-con d-flex justify-content-center">
+      <nav className="navbar navbar-light"> Employee Directory</nav>
+    </div>
+  );
+};
+
+export default Title;
